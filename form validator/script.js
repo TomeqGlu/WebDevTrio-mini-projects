b8cadbaa26e898b991516fb014d1445e1828e756
Tomeq <tomaszglu95@hotmail.com>
2020-04-01 20:15:54 +0200

    Form Validator: beter validation

--- form validator/script.js
+++ form validator/script.js
@@ -19,41 +19,51 @@ function showSuccess(input) {
 }
 
 //Check email is valid
-
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
+// Check required fields
+function checkRequired (inputArr) {
+  inputArr.forEach( function (input) {
+    if(input.value.trim() === '') {
+      showError(input, `${input.id} is required`);
+    } else {
+      showSuccess(input);
+    }
+  }) 
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(input, `${input.id} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${input.id} must be less then ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check password match
+function chcekPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords dont match');
+  }
+}
 
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  if(username.value ==='') {
-    showError(username, 'Username is required');
-  } 
-  else {
-    showSuccess(username);
-  }
-
-  if(email.value ==='') {
-    showError(email, 'Email is required');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
-
-  if(password.value ==='') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if(password2.value ==='') {
-    showError(password2, 'Password 2 is required');
-  } else {
-    showSuccess(password2);
-  }
-})
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  chcekPasswordMatch(password, password2);
+});
