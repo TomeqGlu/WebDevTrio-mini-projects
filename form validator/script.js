f1252a63053fce1dd73bd54e6505b812a0a737c0
Tomeq <tomaszglu95@hotmail.com>
2020-04-09 12:47:37 +0200

    FormValidator: coplete

--- form validator/script.js
+++ form validator/script.js
@@ -6,37 +6,33 @@ const password2 = document.getElementById('password2');
 
 //Show input error message
 function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+  const wrapper = input.parentNode;
+  const errorNode = wrapper.querySelector('.error-message');
+  input.classList.add('error');
+  input.classList.remove('success');
+  errorNode.innerHTML = message;
+  console.log(message);
 }
 
 // Show succes outline
 function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success'
+  const wrapper = input.parentNode;
+  const errorNode = wrapper.querySelector('.error-message');
+
+  input.classList.remove('error');
+  input.classList.add('success');
+  errorNode.innerText = '';
 }
 
 //Check email is valid
 function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
   if(re.test(input.value.trim())) {
-    showSuccess(input);
+    showSuccess(input);    
   } else {
     showError(input, 'Email is not valid');
+    
   }
-}
-
-// Check required fields
-function checkRequired (inputArr) {
-  inputArr.forEach( function (input) {
-    if(input.value.trim() === '') {
-      showError(input, `${input.id} is required`);
-    } else {
-      showSuccess(input);
-    }
-  }) 
 }
 
 // Check input length
@@ -57,13 +53,25 @@ function chcekPasswordMatch(input1, input2) {
   }
 }
 
+// Check required fields
+function ValidateForm (inputArr) {
+  inputArr.forEach( function (input) {
+    if(input.value.trim() === '') {
+      showError(input, `${input.id} is required`);
+    } else {
+      checkLength(username, 3, 15);
+      checkLength(password, 6, 25);
+      checkLength(password2, 6, 25);
+      checkEmail(email);
+      chcekPasswordMatch(password, password2);
+    }
+  }) 
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
-  checkEmail(email);
-  chcekPasswordMatch(password, password2);
+  ValidateForm ([username, email, password, password2]); 
+  
 });
