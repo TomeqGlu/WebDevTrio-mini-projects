8e77d10039705d94716c703846f6051c32f7efc6
Tomeq <tomaszglu95@hotmail.com>
2020-04-13 20:37:09 +0200

    MovieSeatBooking: ading changes to local storage and populate UI

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -5,18 +5,50 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+// Save selected movie index & price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMovieprice', moviePrice);
+}
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.cinema__seat--selected');
   
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat)); //map works like forEach but return array
+  console.log(seatsIndex);
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('cinema__seat--selected');
+      }
+    })
+  }
+
+  selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+  console.log(selectedSeats);
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
@@ -30,5 +62,8 @@ cinema.addEventListener('click', (e) => {
 
     updateSelectedCount();
   }
-})
+});
+
+//Initial cound and total set
+updateSelectedCount(); //There is error with price, after reload in browser always set price to 10$ (first item)
 
